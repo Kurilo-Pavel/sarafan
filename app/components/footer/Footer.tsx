d53b2804b469e197f1a7625951ca4c1606ac7a08
Pavel <kurilo.pavel@mail.ru>
2024-01-27 22:07:02 +0300

    fix 6

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -39,7 +39,6 @@ const Footer = () => {
         title="Компания"
         classNameUl="footer_column"
         classNameTitle="footer_listName"
-        myPage={true}
       />
       <List
         list={Media}
@@ -47,7 +46,6 @@ const Footer = () => {
         title="Следите за нами"
         classNameUl="footer_column"
         classNameTitle="footer_listName"
-        myPage={false}
       />
     </div>
     <p className="footer_text">Все права защищены.Пользовательское соглашение.Политика конфиденциальности
