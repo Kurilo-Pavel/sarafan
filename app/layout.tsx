71a7a8cd68f0b02c93591af245a50fa215496efe
Pavel <kurilo.pavel@mail.ru>
2024-02-04 00:14:27 +0300

    add icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,21 @@
 import "./styles/layout.css";
+import type {Metadata} from 'next'
 import {StoreProvider} from "@/app/store/StoreProvider";
 import Header from "@/app/components/header/Header";
 import Footer from "@/app/components/footer/Footer";
 import React from "react";
 
-export const metadata = {
+export const metadata: Metadata = {
+  openGraph: {
+    title: 'Sarafan - project',
+    description: 'online shop',
+    images: "@/public/png/big/10.jpg"
+  },
   title: "Sarafan",
   description: "internet-shop",
 }
 
-const RootLayout = ({children}: { children: React.ReactNode })=> {
+const RootLayout = ({children}: { children: React.ReactNode }) => {
 
   return (
 
